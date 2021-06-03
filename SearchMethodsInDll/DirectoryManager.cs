6ffab99c596b594de40b36eb9b7cae1300c98367
Dimka Bragin <djremax@yandex.ru>
2021-06-03 11:19:21 +0700

    Initial commit

---
+++ SearchMethodsInDll/DirectoryManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchMethodsInDll
+{
+    class DirectoryManager
+    {
+        public static string[] SearchDllInDirectory(string path)
+        {
+            string[] _dllFiles;
+
+            if (Directory.Exists(path))
+            {
+               return _dllFiles = Directory.GetFiles(path, "*.dll");
+            }
+            else
+            {
+                throw new DirectoryNotFoundException("Директория не найдена");
+            }
+        }
+
+        public static string SearchPublicAndProtectedMethods(string[] files)
+        {
+            //TODO: Обработать исключения
+            string str = "";
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                var assembly = Assembly.Load(files[i]);
+
+                //str += $"{assembly.GetName()} \n";
+
+                foreach (Type type in assembly.GetTypes())
+                {
+                    str += $"{type.Name} \n";
+
+                    foreach (MethodInfo method in type.GetMethods())
+                    {
+                        if (method.IsPublic || method.IsFamily)
+                        {
+                            str += $"\t{method.Name} \n";
+                        }
+                    }
+                }
+            }
+
+            return str;
+        }
+    }
+}
---
+++ SearchMethodsInDll/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Forms;
+
+namespace SearchMethodsInDll
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ChooseFolderPathBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new FolderBrowserDialog();
+            var result = dialog.ShowDialog();
+
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                FolderPathTextBlock.Text = dialog.SelectedPath;
+
+                OutputTextBlock.Text = DirectoryManager.SearchPublicAndProtectedMethods(
+                    DirectoryManager.SearchDllInDirectory(dialog.SelectedPath));
+            }
+        }
+    }
+}
