5f3c8fa204b441082a39c2538b4d1b0c799869ee
Dimka Bragin <djremax@yandex.ru>
2021-06-03 11:38:42 +0700

    Add project for tests

---
+++ TestLibrary/Class1.cs
@@ -0,0 +1,20 @@
+﻿namespace TestLibrary
+{
+    public class Class1
+    {
+        public void Method1 ()
+        {
+
+        }
+
+        protected void Method2 ()
+        {
+
+        }
+
+        private void Method3 ()
+        {
+
+        }
+    }
+}
---
+++ TestLibrary/Class2.cs
@@ -0,0 +1,20 @@
+﻿namespace TestLibrary
+{
+    class Class2
+    {
+        public void Method1()
+        {
+
+        }
+
+        protected void Method2()
+        {
+
+        }
+
+        private void Method3()
+        {
+
+        }
+    }
+}
