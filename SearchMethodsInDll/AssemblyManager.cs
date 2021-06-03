673ec14b9a283245faebd31705dac7e2c38a7be5
Dimka Bragin <djremax@yandex.ru>
2021-06-03 13:09:01 +0700

    Separate the DirectoryManager class according a functionality
    Each class should do own functions.
    * Searching files in directory - DirectoryManager class
    * Working with assembly - AssemblyManager class

---
+++ SearchMethodsInDll/AssemblyManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Reflection;
+
+namespace SearchMethodsInDll
+{
+    class AssemblyManager
+    {
+        /// <summary>
+        /// Search public and protected methods in the *.dll files
+        /// </summary>
+        /// <param name="files">Array of *.dll file names</param>
+        /// <returns> String with classes and class's private and protected methods</returns>
+        public static string SearchPublicAndProtectedMethods(string[] files)
+        {
+            string str = "";
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                var assembly = Assembly.LoadFile(files[i]);
+
+                foreach (Type type in assembly.GetTypes())
+                {
+                    str += $" {type.Name} \n";
+
+                    foreach (MethodInfo method in type.GetMethods
+                        (BindingFlags.Public | BindingFlags.Instance |
+                        BindingFlags.DeclaredOnly | BindingFlags.NonPublic))
+                    {
+                        if (method.IsPublic || method.IsFamily)
+                        {
+                            str += $"\t{method.Name} \n";
+                        }
+                    }
+                }
+            }
+
+            return str;
+        }
+    }
+}
--- SearchMethodsInDll/DirectoryManager.cs
+++ SearchMethodsInDll/DirectoryManager.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Reflection;
+﻿using System.IO;
 
 namespace SearchMethodsInDll
 {
@@ -23,39 +21,6 @@ namespace SearchMethodsInDll
             {
                 throw new DirectoryNotFoundException("Директория не найдена");
             }
-        }
-
-        /// <summary>
-        /// Search public and protected methods in the *.dll files
-        /// </summary>
-        /// <param name="files">Array of *.dll file names</param>
-        /// <returns> String with classes and class's private and protected methods</returns>
-        public static string SearchPublicAndProtectedMethods(string[] files)
-        {
-            //TODO: Обработать исключения
-            string str = "";
-
-            for (int i = 0; i < files.Length; i++)
-            {
-                var assembly = Assembly.LoadFile(files[i]);
-
-                foreach (Type type in assembly.GetTypes())
-                {
-                    str += $"{type.Name} \n";
-
-                    foreach (MethodInfo method in type.GetMethods
-                        (BindingFlags.Public | BindingFlags.Instance | 
-                        BindingFlags.DeclaredOnly | BindingFlags.NonPublic))
-                    {
-                        if (method.IsPublic || method.IsFamily)
-                        {
-                            str += $"\t{method.Name} \n";
-                        }
-                    }
-                }
-            }
-
-            return str;
         }
     }
 }
